2b935969f4951dcef7df9d45ccf7c363a71d1a45
Julian Rüth <julian.rueth@fsfe.org>
2019-01-23 19:55:38 +0100

    fixup

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,5 @@
-echo copy %RECIPE_DIR%\windows\CMakeLists.gen_gmp_aux.txt ntl\CMakeLists.txt
 copy %RECIPE_DIR%\windows\CMakeLists.gen_gmp_aux.txt ntl\CMakeLists.txt
+copy %RECIPE_DIR%\windows\gen_gmp_aux.cpp ntl\gen_gmp_aux.cpp
 
 cmake -G "%CMAKE_GENERATOR%" ^
       -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
@@ -11,6 +11,7 @@ cmake --build .
 
 gen_gmp_aux.exe > ntl/include/NTL/gmp_aux.h
 del gen_gmp_aux.exe
+del ntl\gen_gmp_aux.cpp
 
 copy %RECIPE_DIR%\windows\CMakeLists.txt ntl\CMakeLists.txt
 
