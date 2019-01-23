bfef2d55a3df28ac42bcddbd445bf7116db88f42
Isuru Fernando <isuruf@gmail.com>
2019-01-23 13:40:22 -0600

    one project

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,21 +1,4 @@
-mkdir gen
-cd gen
-
-copy %RECIPE_DIR%\windows\CMakeLists.gen_gmp_aux.txt CMakeLists.txt
-copy %RECIPE_DIR%\windows\gen_gmp_aux.cpp gen_gmp_aux.cpp
-
-cmake -G "%CMAKE_GENERATOR%" ^
-      -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
-      -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
-      -DCMAKE_BUILD_TYPE=Release ^
-      .
-
-cmake --build .
-
-Release\gen_gmp_aux.exe > ..\include\NTL\gmp_aux.h
-
-cd ..
-
+copy %RECIPE_DIR%\windows\gen_gmp_aux.cpp src\gen_gmp_aux.cpp
 copy %RECIPE_DIR%\windows\CMakeLists.txt CMakeLists.txt
 
 cmake -G "%CMAKE_GENERATOR%" ^
