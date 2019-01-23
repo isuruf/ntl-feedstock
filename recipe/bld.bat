298db52aebec85a3485c151532319e8ee0ba4b52
Isuru Fernando <isuruf@gmail.com>
2019-01-23 13:03:48 -0600

    Update bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,24 +1,27 @@
-copy %RECIPE_DIR%\windows\CMakeLists.gen_gmp_aux.txt ntl\CMakeLists.txt
-copy %RECIPE_DIR%\windows\gen_gmp_aux.cpp ntl\gen_gmp_aux.cpp
+mkdir gen
+cd gen
+
+copy %RECIPE_DIR%\windows\CMakeLists.gen_gmp_aux.txt CMakeLists.txt
+copy %RECIPE_DIR%\windows\gen_gmp_aux.cpp gen_gmp_aux.cpp
 
 cmake -G "%CMAKE_GENERATOR%" ^
       -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
       -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
       -DCMAKE_BUILD_TYPE=Release ^
-      ntl
+      .
 
 cmake --build .
 
-gen_gmp_aux.exe > ntl/include/NTL/gmp_aux.h
-del gen_gmp_aux.exe
-del ntl\gen_gmp_aux.cpp
+Release\gen_gmp_aux.exe > ..\include\NTL\gmp_aux.h
 
-copy %RECIPE_DIR%\windows\CMakeLists.txt ntl\CMakeLists.txt
+cd ..
+
+copy %RECIPE_DIR%\windows\CMakeLists.txt CMakeLists.txt
 
 cmake -G "%CMAKE_GENERATOR%" ^
       -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
       -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
       -DCMAKE_BUILD_TYPE=Release ^
-      ntl
+      .
 
 cmake --build . --target install --config Release
